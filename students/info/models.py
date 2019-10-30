a3dc8ffc7e48f5df42e51c85667cd02076338d03
johnsn27 <nathan.johnson@bbc.co.uk>
2019-10-30 15:03:11 +0000

    Add Student model

--- students/info/models.py
+++ students/info/models.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Students(models.Model):
+    Female = 'F'
+    Male = 'M'
+    choices = ((Female, 'Female'), (Male, 'Male'))
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=choices, default=Female)
