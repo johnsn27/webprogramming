1ee353936da6dc880c20290ee14a82d50781de44
johnsn27 <nathan.johnson@bbc.co.uk>
2019-10-30 19:27:59 +0000

    TODO

--- lab2/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # ex: /polls/
-    path('get', views.get, name='get'),
-    path('', views.create_post, name='create'),
-]
