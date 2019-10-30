f879b79878f7b357d5d6691d0a19af8d2798d59b
johnsn27 <nathan.johnson@bbc.co.uk>
2019-10-30 15:03:55 +0000

    Add views for student

--- students/info/views.py
+++ students/info/views.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Students
+from .serializers import studentSerializer
 
-# Create your views here.
+
+class Get_students_List(APIView):
+    def get(self, request):
+        students = Students.objects.all()
+        serialized = studentSerializer(students, many=True)
+        return Response(serialized.data)
+
+
+def homepage(request):
+    return render(request, 'index.html')
